545b7531aeebbdbd06a4f56789a36708abd87b3b
AlexSahion <saha.las399@gmail.com>
2024-08-08 17:38:02 +0200

    new: tsx of Expirience component

---
+++ src/components/home/expirience/Expirience.tsx
@@ -0,0 +1,31 @@
+export default function Expirience() {
+	return (
+		<section className="expirience">
+			<div className="expirience__container">
+				<div className="expirience__titles">
+					<div className="expirience__title title">Один перевозчик <span>в любом городе</span> России</div>
+					<div className="expirience__subtitle subtitle">Почему мы?</div>
+				</div>
+				<div className="expirience__item _icon-ok">
+					На Работу 24 является лидером в сфере организации
+					и предоставления услуг по перевозке сотрудников
+				</div>
+				<div className="expirience__content">
+					<div className="expirience__achievements">
+						<div className="expirience__achievement">
+							<div className="expirience__num">14+</div>
+							<div className="expirience__text">Лет перевозим сотрудников</div>
+						</div>
+						<div className="expirience__achievement">
+							<div className="expirience__num">700+</div>
+							<div className="expirience__text">Довольных клиентов по всей России</div>
+						</div>
+					</div>
+					<div className="expirience__map">
+						<img src="/img/expirience/map.png" alt="img" />
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
