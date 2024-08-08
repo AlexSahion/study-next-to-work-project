5d302a5c869849cf5023124284a1f83c65d517b8
AlexSahion <saha.las399@gmail.com>
2024-08-08 17:37:09 +0200

    add: import jsx expirience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Promo from "@/components/home/promo/Promo";
-
+import Expirience from "@/components/home/expirience/Expirience";
 export default function Home() {
 	return (
 		<>
 			<Promo />
+			<Expirience />
 		</>
 	);
 }
