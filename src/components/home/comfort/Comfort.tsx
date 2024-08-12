451b83728a5567f37bcde81dbcaf9467b0065c4e
AlexSahion <saha.las399@gmail.com>
2024-08-12 12:36:48 +0200

    new: tsx of comfort component

---
+++ src/components/home/comfort/Comfort.tsx
@@ -0,0 +1,53 @@
+export default function Comfort() {
+	return (
+		<section className="comfort">
+			<div className="comfort__container">
+				<div className="comfort__titles titles">
+					<div className="comfort__title title"><span>Делаем комфортной перевозку</span>  ваших сотрудников</div>
+					<div className="comfort__subtitle subtitle">Преимущества</div>
+				</div>
+				<div className="comfort__content">
+					<div className="comfort__item item-comfort">
+						<div className="item-comfort__content">
+							<div className="item-comfort__title">Транспорт всегда в исправном состоянии</div>
+							<ul className="item-comfort__list">
+								<li className="_icon-ok">Механик проверяет каждое транспортное средство перед поездкой на наличие неисправностей</li>
+								<li className="_icon-ok">Каждая машина продублирована и в случае форс-мажора может быть заменена на аналогичное транспортное средство</li>
+								<li className="_icon-ok">Все автобусы оснащены GPS, чтобы вы и ваши сотрудники могли в любое время отслеживать местоположение транспорта в мобильном приложении</li>
+							</ul>
+						</div>
+						<div className="item-comfort__img">
+							<img src="/img/comfort/car.png" alt="img" />
+						</div>
+					</div>
+					<div className="comfort__item item-comfort item-comfort-reverse">
+						<div className="item-comfort__content">
+							<div className="item-comfort__title">Транспорт всегда в исправном состоянии</div>
+							<ul className="item-comfort__list">
+								<li className="_icon-ok">Механик проверяет каждое транспортное средство перед поездкой на наличие неисправностей</li>
+								<li className="_icon-ok">Каждая машина продублирована и в случае форс-мажора может быть заменена на аналогичное транспортное средство</li>
+								<li className="_icon-ok">Все автобусы оснащены GPS, чтобы вы и ваши сотрудники могли в любое время отслеживать местоположение транспорта в мобильном приложении</li>
+							</ul>
+						</div>
+						<div className="item-comfort__img">
+							<img src="/img/comfort/help.png" alt="img" />
+						</div>
+					</div>
+					<div className="comfort__item item-comfort item-comfort-image-end">
+						<div className="item-comfort__content">
+							<div className="item-comfort__title">Транспорт всегда в исправном состоянии</div>
+							<ul className="item-comfort__list">
+								<li className="_icon-ok">Механик проверяет каждое транспортное средство перед поездкой на наличие неисправностей</li>
+								<li className="_icon-ok">Каждая машина продублирована и в случае форс-мажора может быть заменена на аналогичное транспортное средство</li>
+								<li className="_icon-ok">Все автобусы оснащены GPS, чтобы вы и ваши сотрудники могли в любое время отслеживать местоположение транспорта в мобильном приложении</li>
+							</ul>
+						</div>
+						<div className="item-comfort__img">
+							<img src="/img/comfort/clock.png" alt="img" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
