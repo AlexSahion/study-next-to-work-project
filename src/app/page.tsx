639f7bad1501b3ed7d8b5f9527a2b51a14f5adec
AlexSahion <saha.las399@gmail.com>
2024-08-12 12:34:02 +0200

    add: comfort block in home pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Promo from "@/components/home/promo/Promo";
 import Expirience from "@/components/home/expirience/Expirience";
+import Comfort from "@/components/home/comfort/Comfort";
+
 export default function Home() {
 	return (
 		<>
 			<Promo />
 			<Expirience />
+			<Comfort />
 		</>
 	);
 }
