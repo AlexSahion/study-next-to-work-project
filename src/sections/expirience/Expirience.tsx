8b65a275ac6a1433ae15872a5a32927225a94fc1
AlexSahion <saha.las399@gmail.com>
2024-10-02 14:03:24 +0200

    fix: decomposition of expirience block

--- src/sections/expirience/Expirience.tsx
+++ src/sections/expirience/Expirience.tsx
@@ -1,41 +1,44 @@
-import styles from './expirience.module.scss';
+import css from './expirience.module.scss';
+import { CircleCheck } from 'lucide-react';
+import { BaseContainer } from '@/shared/ui';
 
-export default function Expirience() {
+export const Expirience = () => {
 	return (
-		<section className={styles.expirience}>
-			<div className={styles.expirience__container}>
-				<div className={styles.titles}>
-					<div className={styles.title}>
+		<section className={css.expirience}>
+			<BaseContainer className={css.container}>
+				<div className={css.titles}>
+					<div className={css.title}>
 						Один перевозчик <span>в любом городе</span> России
 					</div>
-					<div className={styles.subtitle}>
+					<div className={css.subtitle}>
 						Почему мы?
 					</div>
 				</div>
-				<div className={`${styles.item} _icon-ok`}>
+				<div className={css.item}>
+					<CircleCheck size={25} />
 					На Работу 24 является лидером в сфере организации
 					и предоставления услуг по перевозке сотрудников
 				</div>
-				<div className={styles.content}>
-					<div className={styles.achievements}>
-						<div className={styles.achievement}>
-							<div className={styles.num}>14+</div>
-							<div className={styles.text}>
+				<div className={css.content}>
+					<div className={css.achievements}>
+						<div className={css.achievement}>
+							<div className={css.num}>14+</div>
+							<div className={css.text}>
 								Лет перевозим сотрудников
 							</div>
 						</div>
-						<div className={styles.achievement}>
-							<div className={styles.num}>700+</div>
-							<div className={styles.text}>
+						<div className={css.achievement}>
+							<div className={css.num}>700+</div>
+							<div className={css.text}>
 								Довольных клиентов по всей России
 							</div>
 						</div>
 					</div>
-					<div className={styles.map}>
+					<div className={css.map}>
 						<img src="/img/expirience/map.png" alt="img" />
 					</div>
 				</div>
-			</div>
-		</section>
+			</BaseContainer>
+		</section >
 	);
 }
