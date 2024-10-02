08955400459407c8bc4aca0c8bbd06fd9448ca73
AlexSahion <saha.las399@gmail.com>
2024-10-02 14:07:28 +0200

    add: nav links const

---
+++ src/shared/ui/nav-links/nav-links.ts
@@ -0,0 +1,18 @@
+import { routerMap } from "@/shared/config"
+
+export const navLinks = [
+	{
+		name: 'Автопарк',
+		href: routerMap.autopark,
+	},
+
+	{
+		name: 'Услуги',
+		href: routerMap.services,
+	},
+
+	{
+		name: 'Контакты',
+		href: routerMap.contacts,
+	}
+] 
