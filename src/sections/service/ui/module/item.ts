96b23a66dacfd1a315fc6b4f389bd5434a428693
AlexSahion <saha.las399@gmail.com>
2024-10-04 20:25:27 +0200

    new: item

---
+++ src/sections/service/ui/module/item.ts
@@ -0,0 +1,31 @@
+export const item = [
+	{
+		title: 'Доставка на склад',
+		img: '/img/service/warehouse.png',
+	},
+
+	{
+		title: 'Доставка на вахты',
+		img: '/img/service/shifts.png',
+	},
+
+	{
+		title: 'Доставка на стройки',
+		img: '/img/service/building.png',
+	},
+
+	{
+		title: 'Доставка на ночные смены',
+		img: '/img/service/night-shift.png',
+	},
+
+	{
+		title: 'Доставка в логистические центры',
+		img: '/img/service/centers.png',
+	},
+
+	{
+		title: 'Доставка на заводы и производство',
+		img: '/img/service/factory.png',
+	},
+]
