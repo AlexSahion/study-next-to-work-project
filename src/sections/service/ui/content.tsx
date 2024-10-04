428d95d2c5397ef7b656f00d1faf734f4c7eb2ac
AlexSahion <saha.las399@gmail.com>
2024-10-04 20:25:48 +0200

    new: content block

---
+++ src/sections/service/ui/content.tsx
@@ -0,0 +1,22 @@
+import css from './content.module.scss'
+import { routerMap } from '@/shared/config'
+import { ArrowUpRight } from 'lucide-react'
+import { item } from './module/item'
+
+export const Content = () => {
+	return (
+		<div className={css.content}>
+			{item.map(({ title, img }) => (
+				<div key={img} className={css.item}>
+					<div className={css.img}>
+						<img src={img} alt="img" />
+					</div>
+					<div className={css.bottom}>
+						<div className={css.title}>{title}</div>
+						<div className={css.button}><a href='#' className={css.link}><ArrowUpRight size={19} /></a></div>
+					</div>
+				</div>
+			))}
+		</div>
+	)
+}
