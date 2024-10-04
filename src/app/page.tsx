381e7dfb03e4030bb08f3bd1e0d6441d69130319
AlexSahion <saha.las399@gmail.com>
2024-10-04 20:23:43 +0200

    add: connection blocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { Promo } from "@/sections/promo";
 import { Expirience } from "@/sections/expirience";
 import { Comfort } from "@/sections/comfort";
+import { ProblemSolving } from "@/sections";
+import { Service } from "@/sections";
 
 export default function Home() {
 	return (
 		<>
 			<Promo />
 			<Expirience />
+			<ProblemSolving />
 			<Comfort />
+			<Service />
 		</>
 	);
 }
