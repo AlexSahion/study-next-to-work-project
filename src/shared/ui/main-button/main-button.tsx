aeca6bcdfca6f22b3040a4bffc9f021ba1772344
AlexSahion <saha.las399@gmail.com>
2024-12-05 15:38:20 +0100

    add: functional for popup

--- src/shared/ui/main-button/main-button.tsx
+++ src/shared/ui/main-button/main-button.tsx
@@ -1,16 +1,17 @@
 import css from './main-button.module.scss';
 
 interface MainButtonProps {
+	onClick?: () => void;
 	text?: string;
 	className?: string;
 	icon?: React.ReactNode;
 }
 
-export const MainButton = ({ text = '', className = '', icon }: MainButtonProps) => {
+export const MainButton = ({ onClick, text = '', className = '', icon }: MainButtonProps) => {
 	const combinedClassName = `${css['main-button']} ${className ? className : ''}`.trim();
 
 	return (
-		<button className={combinedClassName}>
+		<button onClick={onClick} className={combinedClassName}>
 			{icon && <span className={css.icon}>{icon}</span>}
 			{text}
 		</button>
