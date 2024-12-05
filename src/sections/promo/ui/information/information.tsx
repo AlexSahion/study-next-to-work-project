7bfcfe450a92d41e373f6c0b56a2994ee7daa3fc
AlexSahion <saha.las399@gmail.com>
2024-12-05 15:38:10 +0100

    add: context

--- src/sections/promo/ui/information/information.tsx
+++ src/sections/promo/ui/information/information.tsx
@@ -1,18 +1,22 @@
+'use client'
+
 import css from './information.module.scss'
 import { Calculator } from 'lucide-react'
 import { MainButton } from '@/shared/ui'
+import { useMyContext } from '@/shared/hooks'
 
 export const Information = () => {
-	return (
+	const { isPopupOpen, setIsPopupOpen } = useMyContext()
 
-		<div className={css.information}>
+	return (
+		<div className={css.information} >
 			<h1 className={css.title}>
 				<span>Доставка сотрудников</span> на работу и обратно в Нижнем Новгороде
 			</h1>
 			<div className={css.text}>
 				Местоположение вашего транспорта на любом маршруте в режиме реального времени через мобильное приложение, позволяющим контролировать каждую поездку вашей организации
 			</div>
-			<MainButton icon={<Calculator />} text="Рассчитать стоимость" className={css.btn} />
-		</div>
+			<MainButton onClick={() => setIsPopupOpen(!isPopupOpen)} icon={<Calculator />} text="Рассчитать стоимость" className={css.btn} />
+		</div >
 	)
 }
