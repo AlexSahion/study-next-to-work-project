ca4cc74f3afacdddcb8b98d78e8ba6039fe7b506
AlexSahion <saha.las399@gmail.com>
2025-07-29 12:39:16 +0200

    fix: filter

--- src/sections/assortment/ui/filters/filters.tsx
+++ src/sections/assortment/ui/filters/filters.tsx
@@ -1,33 +1,34 @@
 import css from './filters.module.scss'
+import { ActiveFilterProps } from '../../assortment'
 
-interface FilterProps {
-	toogleFiltre: (filtre: string) => void
+interface FiltersProps {
+	handleActiveFilter: (filter: ActiveFilterProps) => void
 }
 
-export const Filters: React.FC<FilterProps> = ({ toogleFiltre }) => {
+export const Filters: React.FC<FiltersProps> = ({ handleActiveFilter }) => {
 	return (
 		<div className={css.filters}>
-			<div className={css.filter}>
+			<div onClick={() => handleActiveFilter({ type: 'all', description: 100 })} className={css.filter}>
 				<div className={css.titles}>
-					<div onClick={() => toogleFiltre('all')} className={css.title}>Весь транспорт</div>
+					<div className={css.title}>Весь транспорт</div>
 				</div>
 			</div>
-			<div className={css.filter}>
+			<div onClick={() => handleActiveFilter({ type: 'big', description: 60 })} className={css.filter}>
 				<div className={css.titles}>
 					<div className={css.title}>Автобусы</div>
-					<div onClick={() => toogleFiltre('big')} className={css.subtitle}>вместимость до 60 человек</div>
+					<div className={css.subtitle}>вместимость до 60 человек</div>
 				</div>
 			</div>
-			<div className={css.filter}>
+			<div onClick={() => handleActiveFilter({ type: 'middle', description: 20 })} className={css.filter}>
 				<div className={css.titles}>
 					<div className={css.title}>Микроавтобусы</div>
-					<div onClick={() => toogleFiltre('middle')} className={css.subtitle}>вместимость до 20 человек</div>
+					<div className={css.subtitle}>вместимость до 20 человек</div>
 				</div>
 			</div>
-			<div className={css.filter}>
+			<div onClick={() => handleActiveFilter({ type: 'small', description: 9 })} className={css.filter}>
 				<div className={css.titles}>
 					<div className={css.title}>Минивэны</div>
-					<div onClick={() => toogleFiltre('small')} className={css.subtitle}>местимость до 9 человек</div>
+					<div className={css.subtitle}>местимость до 9 человек</div>
 				</div>
 			</div>
 		</div>
