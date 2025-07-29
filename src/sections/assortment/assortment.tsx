aa2ef2c60eb9a7775451fb4c2760d820efc6cdec
AlexSahion <saha.las399@gmail.com>
2025-07-29 12:38:45 +0200

    new: filter for assortment section

--- src/sections/assortment/assortment.tsx
+++ src/sections/assortment/assortment.tsx
@@ -1,26 +1,39 @@
 'use client'
 
 import css from './assortment.module.scss'
-import { useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import { routerMap } from '@/shared/config'
 import { BaseContainer, Titles } from "@/shared/ui"
 import { AllButton } from '@/shared/ui'
 import { AssortmentItems } from './ui/assortment-items'
 import { Filters } from './ui/filters'
 
-export const Assortment = () => {
-	const [activeFiltre, setActiveFiltre] = useState('all')
+export interface ActiveFilterProps {
+	type: string,
+	description: number
+}
 
-	const toogleActiveFiltre = (filtre: string) => {
-		setActiveFiltre(filtre)
-	}
+export const Assortment = () => {
+	const [activeFilter, setActiveFilter] = useState<ActiveFilterProps>({
+		type: 'all',
+		description: 60,
+	})
+
+	const handleActiveFilter = useCallback((filter: ActiveFilterProps) => {
+		console.log('Нажали фильтр:', filter)
+		setActiveFilter(filter)
+	}, [])
+
+	useEffect(() => {
+		console.log(activeFilter)
+	}, [activeFilter])
 
 	return (
 		<section className={css.assortment}>
 			<BaseContainer className={css.container}>
 				<Titles afterSpan='для доставки сотрудников' beforeSpan='' span='Выберите транспорт ' subtitle='Автопарк' />
-				<Filters toogleFiltre={toogleActiveFiltre} />
-				<AssortmentItems activeFilter={activeFiltre} />
+				<Filters handleActiveFilter={handleActiveFilter} />
+				<AssortmentItems activeFilter={activeFilter} />
 				<div className={css.allButton}>
 					<AllButton routerMap={routerMap.autopark} text='Показать весь автопарк' />
 				</div>
