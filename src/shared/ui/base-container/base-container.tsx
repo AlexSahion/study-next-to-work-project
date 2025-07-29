7a16c38d99855df411439994007f61922a7f9760
AlexSahion <saha.las399@gmail.com>
2025-07-29 12:44:49 +0200

    fix: imports

--- src/shared/ui/base-container/base-container.tsx
+++ src/shared/ui/base-container/base-container.tsx
@@ -1,7 +1,7 @@
 import css from './base-container.module.scss'
 import { type HTMLAttributes, type DetailedHTMLProps } from "react"
 import cn from "classnames"
-import classNames from "classnames"
+
 interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
 
 }
