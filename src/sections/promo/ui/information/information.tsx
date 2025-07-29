b60601768684e7f00866df67a7828445ddd3e1f3
AlexSahion <saha.las399@gmail.com>
2025-07-29 12:42:08 +0200

    fix: imports

--- src/sections/promo/ui/information/information.tsx
+++ src/sections/promo/ui/information/information.tsx
@@ -3,11 +3,8 @@
 import css from './information.module.scss'
 import { Calculator } from 'lucide-react'
 import { MainButton } from '@/shared/ui'
-import { useMyContext } from '@/shared/hooks'
 
 export const Information = () => {
-	const { isPopupOpen, setIsPopupOpen } = useMyContext()
-
 	return (
 		<div className={css.information} >
 			<h1 className={css.title}>
@@ -16,7 +13,7 @@ export const Information = () => {
 			<div className={css.text}>
 				Местоположение вашего транспорта на любом маршруте в режиме реального времени через мобильное приложение, позволяющим контролировать каждую поездку вашей организации
 			</div>
-			<MainButton onClick={() => setIsPopupOpen(!isPopupOpen)} icon={<Calculator />} text="Рассчитать стоимость" className={css.btn} />
+			<MainButton icon={<Calculator />} text="Рассчитать стоимость" className={css.btn} />
 		</div >
 	)
 }
