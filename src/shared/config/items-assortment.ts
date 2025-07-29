a0274b217e9208487f73cd56cadf6bedd1288255
AlexSahion <saha.las399@gmail.com>
2025-07-29 12:42:52 +0200

    new: data for items assortment

---
+++ src/shared/config/items-assortment.ts
@@ -0,0 +1,134 @@
+export interface Vehicle {
+	name: string;
+	capacity: number;
+	price: number;
+	img: string;
+}
+
+export const itemsAssortment: Vehicle[] = [
+	{
+		name: 'Scania Irizar',
+		capacity: 60,
+		price: 3450,
+		img: '/img/assortment/car1.png'
+	},
+
+	{
+		name: 'King Long XMQ6120',
+		capacity: 53,
+		price: 3450,
+		img: '/img/assortment/car2.png'
+	},
+
+	{
+		name: 'Mercedes-Benz Sprinter 41320',
+		capacity: 18,
+		price: 2300,
+		img: '/img/assortment/car3.png'
+	},
+
+	{
+		name: 'Hyundai Starex',
+		capacity: 8,
+		price: 1450,
+		img: '/img/assortment/car4.png'
+	},
+
+	{
+		name: 'Yutong',
+		capacity: 48,
+		price: 2750,
+		img: '/img/assortment/car6.png'
+	},
+
+	{
+		name: 'Голаз 5251 Вояж',
+		capacity: 53,
+		price: 2950,
+		img: '/img/assortment/car7.png'
+	},
+
+	{
+		name: 'Higer 6129',
+		capacity: 56,
+		price: 2950,
+		img: '/img/assortment/car8.png'
+	},
+
+	{
+		name: 'King Long XMQ6129',
+		capacity: 51,
+		price: 2950,
+		img: '/img/assortment/car5.png'
+	},
+
+	{
+		name: 'Scania Irizar',
+		capacity: 60,
+		price: 3450,
+		img: '/img/assortment/car1.png'
+	},
+
+	{
+		name: 'King Long XMQ6120',
+		capacity: 53,
+		price: 3450,
+		img: '/img/assortment/car2.png'
+	},
+
+	{
+		name: 'Mercedes-Benz Sprinter 41320',
+		capacity: 18,
+		price: 2300,
+		img: '/img/assortment/car3.png'
+	},
+
+	{
+		name: 'Hyundai Starex',
+		capacity: 8,
+		price: 1450,
+		img: '/img/assortment/car4.png'
+	},
+
+	{
+		name: 'Hyundai Starex',
+		capacity: 8,
+		price: 1600,
+		img: '/img/assortment/car4.png'
+	},
+
+	{
+		name: 'Hyundai Starex',
+		capacity: 8,
+		price: 3450,
+		img: '/img/assortment/car4.png'
+	},
+
+	{
+		name: 'Yutong',
+		capacity: 48,
+		price: 2750,
+		img: '/img/assortment/car6.png'
+	},
+
+	{
+		name: 'Голаз 5251 Вояж',
+		capacity: 53,
+		price: 2950,
+		img: '/img/assortment/car7.png'
+	},
+
+	{
+		name: 'Higer 6129',
+		capacity: 56,
+		price: 2950,
+		img: '/img/assortment/car8.png'
+	},
+
+	{
+		name: 'King Long XMQ6129',
+		capacity: 51,
+		price: 2950,
+		img: '/img/assortment/car5.png'
+	},
+]
